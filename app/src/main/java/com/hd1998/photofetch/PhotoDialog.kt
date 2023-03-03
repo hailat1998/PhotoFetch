1af2d26e3bfa53627653eae8ce7047d713674637
hailat1998 <htemesgen400@gmail.com>
2023-03-03 20:09:07 +0300

    modified:   app/build.gradle 	modified:   app/src/main/java/com/hd1998/photofetch/PhotoDialog.kt 	modified:   app/src/main/java/com/hd1998/photofetch/PhotoShowFragment.kt 	modified:   app/src/main/res/layout/photo_dialog.xml

--- app/src/main/java/com/hd1998/photofetch/PhotoDialog.kt
+++ app/src/main/java/com/hd1998/photofetch/PhotoDialog.kt
@@ -1,8 +1,36 @@
 package com.hd1998.photofetch
 
+import android.app.AlertDialog
+import android.app.Dialog
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.navArgs
+import com.hd1998.photofetch.databinding.PhotoDialogBinding
 
 class PhotoDialog:DialogFragment() {
-private val args: PhotoDialog by navArgs()
+    private val photoViewModel:PhotoViewModel by activityViewModels()
+   private var _binding : PhotoDialogBinding?=null
+    private val binding : PhotoDialogBinding  get()=
+        checkNotNull(_binding){"not null"}
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        _binding=PhotoDialogBinding.inflate(inflater , container , false)
+        return(binding.root)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+   companion object{
+       const val TAG="PHOTODIALOG"
+   }
 }
--- app/src/main/java/com/hd1998/photofetch/PhotoShowFragment.kt
+++ app/src/main/java/com/hd1998/photofetch/PhotoShowFragment.kt
@@ -10,6 +10,7 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import com.hd1998.photofetch.databinding.FragmentPhotoGallaryBinding
 import kotlinx.coroutines.launch
@@ -110,7 +111,7 @@ class PhotoShowFragment : Fragment() {
     }*/
     private fun updateUI(photoUIState: PhotoUIState) : PhotoAdapter{
         val photoAdapter = PhotoAdapter(photoUIState.photoItems ){
-             }
+           findNavController().navigate()}
         return photoAdapter }
 
 
